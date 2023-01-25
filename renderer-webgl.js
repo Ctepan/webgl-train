80cfbc0016b4b148ae40b33b0a1e1b07dd8e6b7b
Stepan <kvantoreps@gmail.com>
2023-01-25 20:15:59 +0300

    replace tabs with spaces

--- renderer-webgl.js
+++ renderer-webgl.js
@@ -1,62 +1,62 @@
 export async function initRenderer(canvas) {
-	const gl = canvas.getContext('webgl', { alpha: false, antialias: false });
-	gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-	gl.clearColor(0.8, 0.8, 0.8, 1);
+  const gl = canvas.getContext('webgl', { alpha: false, antialias: false });
+  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+  gl.clearColor(0.8, 0.8, 0.8, 1);
 
-	const program = gl.createProgram();
+  const program = gl.createProgram();
 
-	{
-		const shader = gl.createShader(gl.VERTEX_SHADER);
-		const source = await (await fetch('vert.glsl')).text();
-		gl.shaderSource(shader, source);
-		gl.compileShader(shader);
-		console.log(gl.getShaderInfoLog(shader));
-		gl.attachShader(program, shader);
-	}
+  {
+    const shader = gl.createShader(gl.VERTEX_SHADER);
+    const source = await (await fetch('vert.glsl')).text();
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    console.log(gl.getShaderInfoLog(shader));
+    gl.attachShader(program, shader);
+  }
 
-	{
-		const shader = gl.createShader(gl.FRAGMENT_SHADER);
-		const source = await (await fetch('frag.glsl')).text();
-		gl.shaderSource(shader, source);
-		gl.compileShader(shader);
-		console.log(gl.getShaderInfoLog(shader));
-		gl.attachShader(program, shader);
-	}
+  {
+    const shader = gl.createShader(gl.FRAGMENT_SHADER);
+    const source = await (await fetch('frag.glsl')).text();
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    console.log(gl.getShaderInfoLog(shader));
+    gl.attachShader(program, shader);
+  }
 
-	gl.linkProgram(program);
+  gl.linkProgram(program);
 
-	gl.useProgram(program);
+  gl.useProgram(program);
 
-	{
-		const attribLocation = gl.getAttribLocation(program, 'coord');
-		gl.enableVertexAttribArray(attribLocation);
-		gl.vertexAttribPointer(
-			attribLocation, // index
-			2, // size (X and Y)
-			gl.FLOAT, // float32 each
-			false, // normalized. Has no effect on float
-			16, // stride
-			0 // start index
-		);
-	}
+  {
+    const attribLocation = gl.getAttribLocation(program, 'coord');
+    gl.enableVertexAttribArray(attribLocation);
+    gl.vertexAttribPointer(
+      attribLocation, // index
+      2, // size (X and Y)
+      gl.FLOAT, // float32 each
+      false, // normalized. Has no effect on float
+      16, // stride (в буффере x, y, dx, dy, а достаем только x, y)
+      0 // start index
+    );
+  }
 
-	function render(data, count, width, height) {
-		if (width !== canvas.width || height !== canvas.height) {
-			canvas.width = width;
-			canvas.height = height;
-			gl.viewport(0, 0, width, height);
+  function render(data, count, width, height) {
+    if (width !== canvas.width || height !== canvas.height) {
+      canvas.width = width;
+      canvas.height = height;
+      gl.viewport(0, 0, width, height);
 
-			{
-				const attribLocation = gl.getAttribLocation(program, 'scale');
-				gl.disableVertexAttribArray(attribLocation);
-				gl.vertexAttrib2f(attribLocation, 2 / canvas.width, -2 / canvas.height);
-			}
-		}
+      {
+        const attribLocation = gl.getAttribLocation(program, 'scale');
+        gl.disableVertexAttribArray(attribLocation);
+        gl.vertexAttrib2f(attribLocation, 2 / canvas.width, -2 / canvas.height);
+      }
+    }
 
-		gl.bufferData(gl.ARRAY_BUFFER, data.subarray(0, count * 4), gl.DYNAMIC_DRAW);
-		gl.clear(gl.COLOR_BUFFER_BIT);
-		gl.drawArrays(gl.POINTS, 0, count);
-	}
+    gl.bufferData(gl.ARRAY_BUFFER, data.subarray(0, count * 4), gl.DYNAMIC_DRAW);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, count);
+  }
 
-	return { render };
+  return { render };
 };
--- vert.glsl
+++ vert.glsl
@@ -5,12 +5,12 @@ attribute vec2 scale;
 varying vec3 pos;
 
 void main() {
-	gl_Position = vec4(
-		coord.x * scale.x,
-		coord.y * scale.y,
-		0,
-		1
-	);
-	gl_PointSize = 3.0;
-	pos = normalize(gl_Position.xyz + 1.);
+    gl_Position = vec4(
+        coord.x * scale.x,
+        coord.y * scale.y,
+        0,
+        1
+    );
+    gl_PointSize = 3.0;
+    pos = normalize(gl_Position.xyz + 1.);
 }
